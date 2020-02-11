2a65c30d66863df47a3764e54b85cc0a84642298
dinhorodrigues <glebson_lg@hotmail.com>
2020-02-10 21:34:59 -0300

    metodo adiciona todos

--- arquivos/todos.js
+++ arquivos/todos.js
@@ -1,3 +1,31 @@
 var listaElment = document.querySelector('#app ul');
 var inputElement = document.querySelector( '#app input');
 var buttonElement =document.querySelector('#app button');
+
+var todos = [
+    "fazer café",
+    "jogar bola",
+    "aprender JS"
+];
+
+function renderizar(){
+
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listaElment.appendChild(todoElement);
+    }
+
+}
+renderizar();
+
+function addTodos(){
+    listaElment.innerHTML = '';
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    inputElement.value ='';
+    renderizar();
+}
+buttonElement.onclick = addTodos;
